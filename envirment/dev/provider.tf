20c2607b78e27eb8f11e655f1e76ec837da6425a
Vimlendu Shekhar Rai <166683719+Vimlendu27@users.noreply.github.com>
2026-06-17 00:13:37 +0530

    Update Azure provider subscription ID

--- envirment/dev/provider.tf
+++ envirment/dev/provider.tf
@@ -13,7 +13,7 @@ terraform {
   }
 }
 
-/*provider "azurerm" {
-  subscription_id = "301ef477-2de6-4ead-b1a7-d9b7965277ac"
+provider "azurerm" {
+  subscription_id = "7d7e4c65-1bfb-4455-b143-d0d76737869b"
   features {}
-}*/
+}
